f070a8c50c10244750edb26d2c2f1dbee9e37939
陈明君 <chenmingjun@laiweitec.com>
2017-12-07 17:48:20 +0800

    构建springboot项目结构

---
+++ src/main/java/org/doubleganse/springboot/piece/SpringbootPieceApplication.java
@@ -0,0 +1,12 @@
+package org.doubleganse.springboot.piece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPieceApplication.class, args);
+	}
+}
